cea2c130213435b8e09347f7d68c58f0adbd78ee
Saurabh Damde <saurabhdamde@gmail.com>
2024-06-10 15:04:31 +0530

    Reactive Angular Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
+  genders = ['Male', 'Female', 'Other'];
+  forbiddenUsernames = ['god', 'devil'];
+
+  ngOnInit(): void {
+    this.myForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          this.checkForbiddenUsernames.bind(this),
+        ]),
+        email: new FormControl(
+          null,
+          [Validators.email, Validators.required],
+          this.checkForbiddenEmails
+        ),
+      }),
+      gender: new FormControl(null, Validators.required),
+      hobbies: new FormArray([]),
+    });
+  }
+
+  onAddHobby() {
+    (<FormArray>this.myForm.get('hobbies')).push(
+      new FormControl(null, Validators.required)
+    );
+  }
+
+  getHobbies() {
+    return <FormArray>this.myForm.get('hobbies');
+  }
+
+  checkForbiddenUsernames(control: FormControl): { [key: string]: boolean } {
+    if (this.forbiddenUsernames.includes(control.value?.toLowerCase())) {
+      return { usernameIsForbidden: true };
+    }
+    return null;
+  }
+
+  checkForbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value.toLowerCase() === 'test@test.com') {
+          resolve({ emailIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1000);
+    });
+    return promise;
+  }
+
+  onSubmit() {
+    console.log(this.myForm);
+    this.myForm.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    FormsModule,
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [ReactiveFormsModule, BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
