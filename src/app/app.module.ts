9c6b9d4bf76363a8161dba8f3f3966bed24cd7b4
Saurabh Damde <saurabhdamde@gmail.com>
2024-06-24 11:50:54 +0530

    Add NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    FormsModule,
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [FormsModule, BrowserModule, StoreModule.forRoot({}, {})],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
