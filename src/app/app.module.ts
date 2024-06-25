a8f4234e8aabb05c839d83330999c0dae5476830
Saurabh Damde <saurabhdamde@gmail.com>
2024-06-25 12:59:27 +0530

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [FormsModule, BrowserModule, StoreModule.forRoot({}, {})],
-  providers: [],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
