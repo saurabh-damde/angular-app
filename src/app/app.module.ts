771cd9d804f757b85b1840d3dc12be839385040f
Saurabh Damde <saurabhdamde@gmail.com>
2024-06-27 11:22:27 +0530

    Modularize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms';
-import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HeaderComponent } from './components/header/header.component';
+import { SharedModule } from './components/shared/shared.module';
+import { TasksModule } from './components/tasks/tasks.module';
 import { UserComponent } from './components/user/user.component';
-import { TasksComponent } from './components/tasks/tasks.component';
-import { TaskComponent } from './components/tasks/task/task.component';
-import { NewTaskComponent } from './components/tasks/new-task/new-task.component';
-import { CardComponent } from './components/shared/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, TaskComponent, NewTaskComponent, CardComponent],
-  imports: [FormsModule, BrowserModule, StoreModule.forRoot({}, {})],
-  providers: [provideAnimationsAsync()],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
+  imports: [BrowserModule, SharedModule, TasksModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/components/tasks/tasks.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './tasks.component';
+
+@NgModule({
+  declarations: [TasksComponent, TaskComponent, NewTaskComponent],
+  imports: [CommonModule, FormsModule, SharedModule],
+  exports: [TasksComponent],
+})
+export class TasksModule {}
